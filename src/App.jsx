f96eecf9eea486e3d79b0a7d29053c672a9f27b0
m-radjabova <@muslimarajabova1997@gmail.com>
2025-03-11 17:18:27 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const App = () => {
     const [tasks, setTasks] = useState([
-        { 
-            id: 1, 
-            text: "Learn Javascript", 
-            completed: false 
+        {
+            id: 1,
+            text: "Learn Javascript",
+            completed: false,
         },
-        { 
-            id: 2, 
-            text: "Learn React", 
-            completed: false 
+        {
+            id: 2,
+            text: "Learn React",
+            completed: false,
         },
-        { 
-            id: 3, 
-            text: "Build a React App", 
-            completed: false 
+        {
+            id: 3,
+            text: "Build a React App",
+            completed: false,
         },
     ]);
 
     const [newTask, setNewTask] = useState("");
     const [filter, setFilter] = useState("all");
+    const inputRef = useRef(null);
 
     const tasksCount = tasks.filter(task => !task.completed).length;
 
@@ -28,10 +29,15 @@ const App = () => {
         if (newTask.trim()) {
             setTasks([
                ...tasks,
-                { id: tasks.length + 1, text: newTask, completed: false },
+                {
+                    id: tasks.length + 1,
+                    text: newTask,
+                    completed: false,
+                },
             ]);
             setNewTask("");
         }
+        inputRef.current.focus();
     };
 
     const toggleTask = (taskId) => {
@@ -47,11 +53,16 @@ const App = () => {
         return true;
     });
 
+    const deleteTask = (taskId) => {
+        setTasks(tasks.filter(task => task.id !== taskId));
+    }
+
     return (
         <div className="container">
             <div className="p-4">
                 <h1>THINGS TO DO</h1>
                 <input
+                    ref={inputRef}
                     className="form-control mb-3"
                     placeholder="Add a new task..."
                     type="text"
@@ -63,14 +74,17 @@ const App = () => {
                 />
                 <div className="inpContainer">
                     {filteredTasks.map((task) => (
-                        <label key={task.id} className="checkbox">
-                            <input 
-                                type="checkbox" 
-                                checked={task.completed} 
-                                onChange={() => toggleTask(task.id)} 
-                            />
-                            {task.text}
-                        </label>
+                        <div key={task.id} className="task-item">
+                            <label className="checkbox">
+                                <input 
+                                    type="checkbox" 
+                                    checked={task.completed} 
+                                    onChange={() => toggleTask(task.id)} 
+                                />
+                                {task.text}
+                            </label>
+                            <i onClick={() => deleteTask(task.id)} className="bi bi-x-lg"></i>
+                        </div>
                     ))}
                 </div>
             </div>
